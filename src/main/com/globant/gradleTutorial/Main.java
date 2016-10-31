03f43f6261f41c7e532da835ca7e2a199bc6f1d1
Maximiliano Gigena <m.gigena@globant.com>
2016-10-31 16:38:20 -0300

    Initial commit

---
+++ src/main/com/globant/gradleTutorial/Main.java
@@ -0,0 +1,8 @@
+package com.globant.gradleTutorial;
+import org.apache.commons.lang3.StringUtils;
+
+public class Main{
+  public static void main(String[] args){
+    System.out.println(StringUtils.swapCase("Hello world!"));
+  }
+}
